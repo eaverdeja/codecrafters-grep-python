a776dde577af1ec4af251c0cc92a4253afe37063
eaverdeja <eaverdeja@gmail.com>
2025-02-12 08:04:22 -0300

    cleanup

--- app/main.py
+++ app/main.py
@@ -7,8 +7,7 @@ import string
 
 def match_at_pos(text: str, pattern: str) -> tuple[bool, str]:
     if pattern.startswith("[") and pattern.endswith("]"):
-        end_of_group = pattern.index("]")
-        rest_of_pattern = pattern[end_of_group + 1 :]
+        rest_of_pattern = pattern[pattern.index("]") + 1 :]
 
         chars = pattern.strip("[]")
         if chars.startswith("^"):
@@ -18,24 +17,25 @@ def match_at_pos(text: str, pattern: str) -> tuple[bool, str]:
         else:
             # Positive character group
             return any(c == text for c in chars), rest_of_pattern
-    if pattern.startswith("\\d"):
+
+    if pattern.startswith(r"\d"):
         digits = string.digits
         match = any(d == text for d in digits)
         if match:
-            return True, pattern.replace("\\d", "", 1)
+            return True, pattern.replace(r"\d", "", 1)
         return False, pattern
-    if pattern.startswith("\\w"):
+
+    if pattern.startswith(r"\w"):
         alphanumerics = string.digits + string.ascii_letters
         match = any(a == text for a in alphanumerics)
         if match:
-            return True, pattern.replace("\\w", "", 1)
+            return True, pattern.replace(r"\w", "", 1)
         return False, pattern
 
     # Literal character
     match = text == pattern[0]
-    if match:
-        return True, pattern[1:]
-    return False, pattern[1:]
+    remaining_pattern = pattern[1:]
+    return match, remaining_pattern
 
 
 def match_pattern(text: str, pattern: str) -> bool:
@@ -43,13 +43,14 @@ def match_pattern(text: str, pattern: str) -> bool:
     match = False
     while pos < len(text):
         match, pattern = match_at_pos(text[pos], pattern)
+        pos += 1
         if pattern:
             # If there's still a pattern to consume
             # it means we don't have a match yet
             match = False
         else:
+            # Else there's nothing more to check
             break
-        pos += 1
 
     return match
 
